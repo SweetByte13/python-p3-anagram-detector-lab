402a599d1778932ce78d0e3bca8574dc6ff7b5ef
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-05-15 15:48:20 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, wordsToCheck):
+        #["e", "i", ...] -> "ei..."
+        ourWordInOrder = ''.join(sorted([letter for letter in self.word]))
+        result = []
+        for wordToCheck in wordsToCheck:
+            theirWordInOrder = ''.join(sorted([letter for letter in wordToCheck]))
+            if(ourWordInOrder == theirWordInOrder):
+                result.append(wordToCheck)
+                
+        return result
+            
+                
+            
+        
+        #ways we can tell if words are anagrams
+        #exclude any word thats not the same length
+        
+        
